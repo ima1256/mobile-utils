5bd6bf72b5d376e999d59e495d139603bda37e08
Imanol Conde González <Imanol Conde>
2025-07-12 20:25:24 +0200

    KeepAlive local

--- keepAlive.js
+++ keepAlive.js
@@ -1,13 +1,28 @@
 const https = require("https");
+const http = require("http");
 
-const URL = "https://factoryflow-backend.onrender.com/api/machines"; // Reemplaza con tu URL
+//const URL = "https://factoryflow-backend.onrender.com/api/ping"; // Reemplaza con tu URL
+//const URL = "http://localhost:3000/api/ping";
+const URL = "http://192.168.1.48:3000/api/ping";
 
-setInterval(() => {
-  https
+const ping = () => {
+  http
     .get(URL, (res) => {
-      console.log(`[${new Date().toISOString()}] Ping: ${res.statusCode}`);
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        console.log(`[${new Date().toISOString()}] Ping: ${res.statusCode}`);
+        console.log(`[${new Date().toISOString()}] Respuesta: ${data}`);
+      });
     })
     .on("error", (err) => {
       console.error(`[${new Date().toISOString()}] Error:`, err.message);
     });
-}, 5 * 60 * 1000); // Cada 5 minutos
+};
+
+ping();
+
+setInterval(ping, 5 * 60 * 1000); // Cada 5 minutos
